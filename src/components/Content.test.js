ffef08e3c37cb84f313446c1a3ca66bedc6f4cfa
Dave Stanton <davestanton.us@gmail.com>
2017-11-22 18:24:39 -0500

    Fixes tests for part 1. (#2)
    * Update Content.test.js
    
    * Update Header.test.js

--- src/components/Content.test.js
+++ src/components/Content.test.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
-import Content from './Content';
+import React from "react";
+import ReactDOM from "react-dom";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import Content from "./Content";
 
-describe('<Content>', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Content />, div);
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Content", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    let setTitle = jest.fn();
+    ReactDOM.render(<Content setTitle={setTitle} />, div);
   });
 
-  it('properly shows given text', () => {
-    const output = shallow(<Content text="Unit Tests" />);
+  it("properly shows given text", () => {
+    let setTitle = jest.fn();
+    const output = Enzyme.shallow(
+      <Content text="Unit Tests" setTitle={setTitle} />
+    );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
-});
 
+});
--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import { shallowToJson } from 'enzyme-to-json';
-import Header from './Header';
+import React from "react";
+import ReactDOM from "react-dom";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+import Header from "./Header";
 
-describe('Header', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Header", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<Header />, div);
   });
 
-  it('should render properly', () => {
-    const output = shallow(<Header title="Unit Tests" />);
+  it("should render properly", () => {
+    const output = Enzyme.shallow(<Header title="Unit Tests" />);
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 
-  it('should handle a click event', () => {
+  it("should handle a click event", () => {
     let props = {
       onHeaderClick: jest.fn(),
-      title: 'Unit Tests'
+      title: "Unit Tests"
     };
 
-    const output = mount(<Header {...props} />);
-    const anchorTag = output.find('#logo');
-    anchorTag.simulate('click');
+    const output = Enzyme.mount(<Header {...props} />);
+    const anchorTag = output.find("#logo");
+    anchorTag.simulate("click");
     expect(props.onHeaderClick).toHaveBeenCalled();
   });
 });
-
